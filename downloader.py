6aba3cdb0afda86c2f522c59267584798d0eb087
fadma charki <fadmaproco@gmail.com>
2025-09-28 02:44:15 +0100

    updated

--- downloader.py
+++ downloader.py
@@ -1,13 +1,13 @@
 from pytube import YouTube
 import os
 
-def download_video(link, progress):
+def download_video(link):
     # save the video in 'downloads' folder after download
     save_path = os.path.join(os.getcwd(), "downloads")
     os.makedirs(save_path, exist_ok=True)  # create the folder if not created before
 
     try:
-        yt = YouTube(link, on_progress_callback=progress)
+        yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
         stream.download(output_path=save_path)
         print(f"Download completed successfully! Video saved to: {save_path}")
